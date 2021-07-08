cafccf91a70a8d29eac0da5564f6aa087e325881
yangxudong-functorz <59385709+yangxudong-functorz@users.noreply.github.com>
2021-07-08 12:01:55 +0800

    Create issues1.ts

---
+++ issues1.ts
@@ -0,0 +1,10 @@
+export default {
+  navigationStyle: 'default',
+  navigationBarBackgroundColor: '#F3F3F3',
+  navigationBarTextStyle: 'black',
+  backgroundColor: '#FFFFFF',
+  backgroundTextStyle: 'dark',
+  disableScroll: true,
+  enableShareAppMessage: false,
+  navigationBarTitleText: '!'
+};
